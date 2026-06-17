a7a517fc53f8f527e109c37ca905e1a1f2c2ba94
DanielRomDom <danielromdom01@gmail.com>
2026-06-17 21:54:52 +0200

    fix auth and register

--- task-frontend/src/pages/Register.jsx
+++ task-frontend/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { registerUser, loginUser } from "../services/auth";
+import { registerUser } from "../services/auth";
 
 export default function Register({ setToken }) {
   const [email, setEmail] = useState("");
